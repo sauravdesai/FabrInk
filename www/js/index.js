1205f0f5b50a3c1e4b59c4d16b461c280567eee3
Varad Gundap <30592638+varad615@users.noreply.github.com>
2021-09-26 23:32:18 +0530

    Files Upload

---
+++ www/js/index.js
@@ -0,0 +1,21 @@
+function onSubmit() {
+  var validate = document.forms["loginform"]["username"].value;
+  var passvalidate = document.forms["loginform"]["password"].value;
+  if (
+    validate == "" ||
+    validate == null ||
+    passvalidate == "" ||
+    passvalidate == null
+  ) {
+    document.getElementById("demo").innerHTML =
+      "Please fill out all the fields";
+  } else {
+    console.log("user named " + validate + " loggedin");
+    alert("Welcome " + validate + ". Press ok to redirect");
+    window.location.href = "https://github.com/varad615";
+  }
+}
+
+function signup() {
+    window.location.href = "pages/signup.html";
+}
